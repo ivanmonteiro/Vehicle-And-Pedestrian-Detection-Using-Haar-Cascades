52afcb84b4a7cfc71c0b894cb5c02810a0157c3d
Ivan Monteiro Cantalice <ivanmonteiroc@gmail.com>
2019-05-31 20:15:28 -0300

    changes to code improve accuracy

--- detection_all.py
+++ detection_all.py
@@ -1,34 +1,39 @@
 # -*- coding: utf-8 -*-
 
 import cv2
+import imutils
 print('Project Topic : Vehicle Classification')
 print('Research Internship on Machine learning using Images')
 print('By Aditya Yogish Pai and Aditya Baliga B')
 
-video_src = 'pedestrians.avi'
+video_src = 'fight.mp4'
 
 cap = cv2.VideoCapture(video_src)
 
-bike_cascade = cv2.CascadeClassifier('pedestrian.xml')
+pedestrians_cascade = cv2.CascadeClassifier('pedestrian.xml')
 car_cascade = cv2.CascadeClassifier('cars.xml')
 
 while True:
     ret, img = cap.read()
-	
+
     if (type(img) == type(None)):
         break
     
+    img = imutils.resize(img, width=300)
+	
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    bike = bike_cascade.detectMultiScale(gray,1.3,2)
 
-    for(a,b,c,d) in bike:
-        cv2.rectangle(img,(a,b),(a+c,b+d),(0,255,210),4)
-    """
-    cars = car_cascade.detectMultiScale(gray, 1.1, 2)
+    pedestrians = pedestrians_cascade.detectMultiScale(gray,1.1,1, minSize=(20,20))
+    #pedestrians = pedestrians_cascade.detectMultiScale(gray,1.1, 1)
+    #cars = car_cascade.detectMultiScale(gray, 1.1, 2)
+    cars = car_cascade.detectMultiScale(gray, 1.1, 3, minSize=(50,50))
+
+    for(a,b,c,d) in pedestrians:
+        cv2.rectangle(img,(a,b),(a+c,b+d),(0,255,255),4)
     
     for (x,y,w,h) in cars:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,255),2)
-    """
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+    
     cv2.imshow('video', img)
     
     if cv2.waitKey(33) == 27:
